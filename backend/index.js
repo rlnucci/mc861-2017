02ea023cb4db911aed7ac4ad290c970cec53dd98
rlnucci <raissanucci@gmail.com>
2017-03-14 17:35:12 -0300

    basic router

---
+++ backend/index.js
@@ -0,0 +1,36 @@
+import Koa from 'koa';
+import router from 'koa-router';
+
+console.log('Starting...');
+
+const rootRouter = router();
+
+const app = new Koa();
+
+// x-response-time
+
+app.use(async function (ctx, next) {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  ctx.set('X-Response-Time', `${ms}ms`);
+});
+
+// logger
+
+app.use(async function (ctx, next) {
+  const start = new Date();
+  await next();
+  const ms = new Date() - start;
+  console.log(`${ctx.method} ${ctx.url} - ${ms}`);
+});
+
+// response
+
+app.use(ctx => {
+  ctx.body = 'Hello World';
+});
+
+app.listen(3000);
+
+console.log('Started!');
